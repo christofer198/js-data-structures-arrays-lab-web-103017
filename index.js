6f1d77fa6b04eda5f7238549b963db56ef44f4ef
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-11 13:01:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ function destructivelyRemoveFirstDriver(){
 }
 
 function appendDriver(name){
-  return [...drivers, name]
+  return [...drivers, name];
+}
+
+function prependDriver(name){
+    return [name, ...drivers];
 }
